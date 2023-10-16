d9f95b8162bbd61f3854e09c109a3f8629b37ed4
Churchillk <132873688+Churchillk@users.noreply.github.com>
2023-10-16 08:51:23 +0300

    created js file and pasted all js script from html to the js

--- back.js
+++ back.js
@@ -1,1 +1,12 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        document.getElementById("info").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
+function showPosition(position) {
+    document.getElementById("info").innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
